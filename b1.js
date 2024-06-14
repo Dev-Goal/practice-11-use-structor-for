48c630e5f74e84e34c56acfaaff8b14a859fc06e
Admin <lmht721@gmail.com>
2024-06-14 16:07:25 +0700

    add bt 11

---
+++ b1.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < 5; i++) {
+  document.write("The Number is " + i + "<br>");
+}
---
+++ bt2.js
@@ -0,0 +1,6 @@
+let number = prompt("Nhập vào một số");
+let tatol = 0;
+for ( let i = 1; i <= number; i++) {
+  tatol += i;
+  
+}alert("Tổng các phần tử từ 1 đến " + number + " là " + tatol);
